e9a561140bec84e60e5bce17f32322d2bf61f4fe
Jidim <78730440+Jidim@users.noreply.github.com>
2022-12-05 03:21:15 +0300

    first

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const a = 1;
 
 function foo(name) {
-  const lastName = name;
-  return lastName;
+	const lastName = name;
+	return lastName;
 }
 foo('Hi');
 const x = 200;
 
-console.log(x, a);
+console.log(a);

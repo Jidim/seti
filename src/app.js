1db7af5be8ed86edc30d93d3990982459f926a5a
Jidim <78730440+Jidim@users.noreply.github.com>
2022-12-05 12:46:40 +0300

    Fourth

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-const a = 1;
-
+let a = 1;
+let m = new Array(2);
+m = [1, 2];
 function foo(name) {
 	const lastName = name;
 	return lastName;
 }
-
+a++;
+// sofjhsrl;fhskdfjrhipesghsd;fljhlj;sdfhgj;d
 foo('Hi, man');
-const x = 200
+const x = 200;
+
+foo(m[0]);
 
 foo(a + x);
